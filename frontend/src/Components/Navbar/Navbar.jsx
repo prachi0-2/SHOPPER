945a579c5bce2bd78511d49b69c03c8715cab958
prachi <prachi@gmail.com>
2024-07-10 18:39:22 +0530

    added

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useContext, useRef, useState } from 'react'
+import "./Navbar.css"
+import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext';
+import nav_dropdown from '../Assests/nav-dropdown.png'
+ const Navbar=() => {
+
+    const[menu,setMenu]=useState("shop");
+    const{getTotalCartItems}=useContext(ShopContext);
+    const menuRef=useRef();
+
+    const dropdown_toggle=(e)=>{
+         menuRef.current.classList.toggle('nav-menu-visible');
+         e.target.classList.toggle('open');
+    }
+  return (
+    <div className="navbar">
+        <div className="nav-logo">
+            <img src="https://img.freepik.com/premium-vector/shopping-bag-illustration-vector_643279-153.jpg" alt=" "/>
+            <p>SHOPPER</p>
+        </div>
+        <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt=""/>
+<ul ref={menuRef} className="nav-menu">
+    <li ><Link to='/'>Shop</Link><hr/></li>
+    <li><Link to='/men'>Men</Link></li>
+    <li ><Link to='/women'>Women</Link></li>
+    <li ><Link to='/kids'>Kids</Link></li>
+</ul>
+<div className="nav-login-cart">
+   <Link to='/login'><button>Login</button></Link> 
+   <Link to='/cart'><img src="https://t3.ftcdn.net/jpg/03/14/84/68/360_F_314846831_5jJsC7Us9obgwMjRDqFhs04dodzvnZvi.jpg" alt=" "/></Link> 
+    <div className="nav-cart-count">{getTotalCartItems()}</div>
+</div>
+    </div>
+  )
+}
+export default Navbar
